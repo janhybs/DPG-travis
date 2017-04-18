b77be1f54c2924b280f8051830a1e43e5a6513a9
Jan Hybš <x3mSpeedy@users.noreply.github.com>
2017-04-18 11:38:07 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,10 @@ print("Python version %s " % str(sys.version))
 print("Cores          %s " % str(mp.cpu_count()))
 
 
-if 1:
+if True:
   print("tady")
 else:
   print("tady nikdy")
+  a = 0
+  b = 1
+  c = b / a
